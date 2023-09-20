5d899dd952276d8e1256c7b798a61515fe976e3d
Snehasharma76 <snehasharma7620@gmail.com>
2023-09-20 12:51:23 +0530

    Billing Address Tab

---
+++ src/app/Components/billingcard/billingcard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billingcard',
+  templateUrl: './billingcard.component.html',
+  styleUrls: ['./billingcard.component.scss']
+})
+export class BillingcardComponent {
+
+}
